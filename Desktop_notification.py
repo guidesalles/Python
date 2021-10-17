694ed58ed0c88ac7ac55788b8f58a2d21c1b95f9
Guilherme Marques de Salles Soares <83288210+guidesalles@users.noreply.github.com>
2021-10-17 17:38:35 -0300

    Add files via upload

---
+++ Desktop_notification.py
@@ -0,0 +1,9 @@
+from plyer import notification
+
+notification.notify(
+    title = "Estudar todo dia.",
+    message = "só vai conseguir aprender Python, fazendo exercício.",
+    app_icon = None,
+    timeout= 20,
+    toast= False)
+
